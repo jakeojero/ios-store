88f768c0f6813cc15e4eedf939fc698b5ee060cf
Jake Ojero <jakeojero@hotmail.com>
2019-07-13 12:34:09 -0400

    Add Table View and cell configurations

---
+++ merch-shop/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  merch-shop
+//
+//  Created by Jake Ojero on 2019-07-13.
+//  Copyright © 2019 jakeojero.me. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage: UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
