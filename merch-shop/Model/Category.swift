7c854ef0e45a37aa1cd643bed55d2547f6a506d3
Jake Ojero <jakeojero@hotmail.com>
2019-07-13 13:40:15 -0400

    Added table view for categories

---
+++ merch-shop/Model/Category.swift
@@ -0,0 +1,19 @@
+//
+//  Category.swift
+//  merch-shop
+//
+//  Created by Jake Ojero on 2019-07-13.
+//  Copyright © 2019 jakeojero.me. All rights reserved.
+//
+
+import Foundation
+
+struct Category {
+    private(set) public var title: String
+    private(set) public var imageName: String
+
+    init(title: String, imageName: String) {
+        self.title = title
+        self.imageName = imageName
+    }
+}
---
+++ merch-shop/Services/DataService.swift
@@ -0,0 +1,24 @@
+//
+//  DataService.swift
+//  merch-shop
+//
+//  Created by Jake Ojero on 2019-07-13.
+//  Copyright © 2019 jakeojero.me. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+    
+    private let categories = [
+        Category(title: "SHIRTS", imageName: "shirts.png"),
+        Category(title: "HOODIES", imageName: "hoodies.png"),
+        Category(title: "HATS", imageName: "hats.png"),
+        Category(title: "DIGITAL", imageName: "digital.png")
+    ]
+    
+    func getCategories() -> Array<Category> {
+        return categories
+    }
+}
