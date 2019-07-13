37e6cf2be11f6ac3c548152055ba5d3c1f115f86
Jake Ojero <jakeojero@hotmail.com>
2019-07-13 14:02:41 -0400

    Add Collection View with products

---
+++ merch-shop/View/ProductCell.swift
@@ -0,0 +1,17 @@
+//
+//  ProductCell.swift
+//  merch-shop
+//
+//  Created by Jake Ojero on 2019-07-13.
+//  Copyright © 2019 jakeojero.me. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTile: UILabel!
+    @IBOutlet weak var productPrive: UILabel!
+    
+}
